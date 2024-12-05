8cb5384d9e877757b00f5ec943888bfcc9fca942
Onur Tanrıkulu <onurtnrkl@users.noreply.github.com>
2024-12-05 14:51:30 +0300

    fix: symlink (#14)
    * fix: symbolic link
    
    Fixes #11
    
    * chore: rename setup script
    
    * Create .gitkeep
    
    * Update setup_godotpackage.sh
    
    * fix: gitkeep addons

--- src/setup_godotpackage.sh
+++ src/setup_godotpackage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ln -s "../../../src/{{Author(P)}}.{{Name(P)}}.Godot/addons/{{Author(P)}}.{{Name(P)}}" "{{Name(P)}}/sandbox/GodotApplication/addons"
+ln -s "../../../src/{{Author(P)}}.{{Name(P)}}.Godot/addons/{{Author(P)}}.{{Name(P)}}" "{{Name(P)}}/sandbox/GodotApplication/addons/{{Author(P)}}.{{Name(P)}}"
 rm -- "$0"
